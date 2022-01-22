55f5cc7541533c34b734c2e0ffcd7911f37bf4e6
PandaTobi <49051766+PandaTobi@users.noreply.github.com>
2022-01-22 09:42:02 -0600

    got the camera working

--- lib/screens/school_home/admin_home_screen.dart
+++ lib/screens/school_home/admin_home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:admin/screens/login/login_screen.dart';
+import 'package:admin/screens/school_home/camera.dart';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
 import '../../constants.dart';
@@ -39,6 +41,25 @@ class _AdminHomeState extends State<AdminHomeScreen> {
             ],
           ),
         ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera_alt),
+        onPressed: () async {
+          WidgetsFlutterBinding.ensureInitialized();
+
+          // Obtain a list of the available cameras on the device.
+          final cameras = await availableCameras();
+
+          // Get a specific camera from the list of available cameras.
+          final firstCamera = cameras.first;
+
+
+          Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => TakePictureScreen(camera: firstCamera))
+          );
+        },
       ),
     );
   }
