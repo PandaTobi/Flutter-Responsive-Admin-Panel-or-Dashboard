c13104c87092c0ffabef0915db3319e99ce1b752
PandaTobi <49051766+PandaTobi@users.noreply.github.com>
2022-03-09 18:49:44 -0600

    3922

--- lib/screens/school_home/face_detector/camera_view.dart
+++ lib/screens/school_home/face_detector/camera_view.dart
@@ -7,7 +7,7 @@ import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:image_picker/image_picker.dart';
 
 
-import '../main.dart';
+import '../../../main.dart';
 
 enum ScreenMode { liveFeed, gallery }
 
